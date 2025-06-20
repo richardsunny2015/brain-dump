cdbd098acd9506cd0a3a3c6d819d7b6d19463546
Richard Lanchez <richardsunny2015@gmail.com>
2025-06-20 13:41:52 -0400

    Add exercises

--- Learn Physics with Functional Programming/Chapter 1 - Calculating With Haskell/code.hs
+++ Learn Physics with Functional Programming/Chapter 1 - Calculating With Haskell/code.hs
@@ -9,10 +9,25 @@ pi;
 
 cos pi;
 
-cos pi/2;
+cos pi/2; -- equivalent to (cos pi) / 2
 
 cos (pi / 2);
 
 pi > 3;
 
 1 + 2 * 3;
+
+cos $ pi / 2; -- equivalent to cos (pi / 2) 
+
+logBase 10 100;
+
+atan2 1 0;
+
+sqrt 5 ^ 2 == 5;
+
+-- Exercises
+-- 1.2
+2 ^ (3 ^ 4);
+(2 / 3) / 4;
+7 - (5 / 4);
+(log 49) / 7;
